b87965ff569d4a780f1994db6852a788fcf73c47
rian <rianwahyu26@gmail.com>
2021-06-14 12:04:48 +0700

    update Fault

--- app/src/main/java/com/bibitproject/aplikasidatachart/Staff/ui/HomeFaultFragment.java
+++ app/src/main/java/com/bibitproject/aplikasidatachart/Staff/ui/HomeFaultFragment.java
@@ -6,24 +6,63 @@ import android.os.Bundle;
 
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bibitproject.aplikasidatachart.R;
+import com.bibitproject.aplikasidatachart.Staff.Model.Monitoring;
+import com.bibitproject.aplikasidatachart.Utils.MyConfig;
+import com.bibitproject.aplikasidatachart.Utils.NetworkState;
 import com.bibitproject.aplikasidatachart.databinding.FragmentHomeFaultBinding;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LegendEntry;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 public class HomeFaultFragment extends Fragment {
 
     FragmentHomeFaultBinding binding;
+
+    List<Monitoring> listMonitoring = new ArrayList<Monitoring>();
+    LinearLayoutManager linearLayoutManager;
+
+    List<String> xLabel = new ArrayList<String>();
+    List<BarEntry> dataCritical = new ArrayList<BarEntry>();
+    List<BarEntry> dataMjor = new ArrayList<BarEntry>();
+    List<BarEntry> dataMinor = new ArrayList<BarEntry>();
+
+    ArrayList<PieEntry> listFaultHistogram = new ArrayList<PieEntry>();
+    ArrayList<Integer> listColors = new ArrayList<Integer>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,7 +71,11 @@ public class HomeFaultFragment extends Fragment {
         //return inflater.inflate(R.layout.fragment_home_fault, container, false);
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home_fault, container, false);
 
-        BarDataSet barDataSet1 = new BarDataSet(barEntries1(), "DataSet 1");
+        listMonitoring = new ArrayList<>();
+
+        listFaultHistogram = new ArrayList<>();
+        listColors = new ArrayList<>();
+        /*BarDataSet barDataSet1 = new BarDataSet(barEntries1(), "DataSet 1");
         barDataSet1.setColor(Color.RED);
         BarDataSet barDataSet2 = new BarDataSet(barEntries2(), "DataSet 2");
         barDataSet2.setColor(Color.BLUE);
@@ -47,10 +90,14 @@ public class HomeFaultFragment extends Fragment {
         String[] days = new String[]{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
         XAxis xAxis = binding.barFault.getXAxis();
 
-        xAxis.setCenterAxisLabels(true);
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setGranularity(1);
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(days));
         xAxis.setGranularityEnabled(true);
+        xAxis.setCenterAxisLabels(true);
+        xAxis.setGranularity(1f);
+        xAxis.setTextSize(10f);
+        xAxis.setTextColor(Color.RED);
+        xAxis.setDrawAxisLine(true);
+        xAxis.setDrawGridLines(false);
 
         binding.barFault.setDragEnabled(true);
         binding.barFault.setVisibleXRangeMaximum(3);
@@ -60,16 +107,222 @@ public class HomeFaultFragment extends Fragment {
         data.setBarWidth(0.10f);
 
         binding.barFault.getXAxis().setAxisMinimum(0);
-        binding.barFault.getXAxis().setAxisMinimum(0 +
-                binding.barFault.getBarData().getGroupWidth(groupSpace, barSpace)*7);
+//        binding.barFault.getXAxis().setAxisMinimum(0 +
+//                binding.barFault.getBarData().getGroupWidth(groupSpace, barSpace)*7);
         binding.barFault.getAxisLeft().setAxisMinimum(0);
 
         binding.barFault.groupBars(0, groupSpace, barSpace);
 
-        xAxis.setValueFormatter(new IndexAxisValueFormatter(days));
+
         binding.barFault.invalidate();
 
+        Legend l = binding.barFault.getLegend();
+        l.setFormSize(10f);
+        l.setForm(Legend.LegendForm.CIRCLE);
+        l.setTextSize(12f);
+        l.setTextColor(Color.BLACK);*/
+
+        getFaultDaily();
+
         return binding.getRoot();
+    }
+
+    private void getFaultDaily() {
+        listMonitoring.clear();
+        //OnShimmer();
+        String url = NetworkState.getUrl()+"staff/getFaultDaily.php";
+        RequestQueue mRequestQueue = Volley.newRequestQueue(getContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("response", response);
+                try {
+                    //menangkap respon JSON dari server
+                    JSONObject jsonObject = new JSONObject(response);
+                    boolean success = jsonObject.getBoolean("success");
+                    String message = jsonObject.getString("message");
+                    //cek status login dari jsonserver
+                    if (success==true){
+                        //OnFoundData();
+                        String data = jsonObject.getString("data");
+                        JSONArray jsonArray = new JSONArray(data.toString());
+                        for (int i=0;i<jsonArray.length();i++){
+                            JSONObject object = jsonArray.getJSONObject(i);
+
+                            String reg = object.getString("reg");
+                            String cri = object.getString("cri");
+                            String maj = object.getString("maj");
+                            String min = object.getString("min");
+
+                            xLabel.add(reg);
+
+                            dataCritical.add(new BarEntry(1f * i, Float.parseFloat(cri)));
+                            dataMjor.add(new BarEntry(1f * i, Float.parseFloat(maj)));
+                            dataMinor.add(new BarEntry(1f * i, Float.parseFloat(min)));
+                        }
+
+                        BarDataSet barCritical = new BarDataSet(dataCritical, "Critical");
+                        barCritical.setColor(Color.RED);
+
+                        BarDataSet barMajor = new BarDataSet(dataMjor, "Major");
+                        barMajor.setColor(Color.YELLOW);
+
+                        BarDataSet barMinor = new BarDataSet(dataMinor, "Minor");
+                        barMinor.setColor(Color.GREEN);
+
+                        BarData barData = new BarData(barCritical, barMajor, barMinor);
+                        binding.barFault.setData(barData);
+
+
+                        XAxis xAxis = binding.barFault.getXAxis();
+
+                        xAxis.setValueFormatter(new IndexAxisValueFormatter(xLabel));
+                        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+                        xAxis.setGranularityEnabled(true);
+                        xAxis.setCenterAxisLabels(true);
+                        xAxis.setGranularity(1f);
+                        xAxis.setTextSize(10f);
+                        xAxis.setTextColor(Color.RED);
+                        xAxis.setDrawAxisLine(true);
+                        xAxis.setDrawGridLines(false);
+
+                        binding.barFault.setDragEnabled(true);
+                        binding.barFault.setVisibleXRangeMaximum(7);
+
+                        float barSpace = 0.08f;
+                        float groupSpace = 0.44f;
+                        barData.setBarWidth(0.10f);
+
+                        binding.barFault.getXAxis().setAxisMinimum(0);
+//        binding.barFault.getXAxis().setAxisMinimum(0 +
+//                binding.barFault.getBarData().getGroupWidth(groupSpace, barSpace)*7);
+                        binding.barFault.getAxisLeft().setAxisMinimum(0);
+
+                        binding.barFault.groupBars(0, groupSpace, barSpace);
+
+
+                        binding.barFault.invalidate();
+
+                        Legend l = binding.barFault.getLegend();
+                        l.setFormSize(10f);
+                        l.setForm(Legend.LegendForm.CIRCLE);
+                        l.setTextSize(12f);
+                        l.setTextColor(Color.BLACK);
+
+                        getFaultHistogram();
+
+                    }else{
+                        MyConfig.showToast(getContext(), message);
+                        //OnEmpty();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //OnEmpty();
+                Log.d("error", error.getMessage().toString());
+                MyConfig.showToast(getContext(), error.getMessage().toString());
+            }
+        }){
+            //parameter untuk login yang akan di tangkap api server
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                /*params.put("userIdPelayan",new SessionManager(getContext()).getIdUser());
+                params.put("stats","0");*/
+                return params;
+            }
+        };
+        int socketTimeout = 30000;
+        RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(policy);
+        mRequestQueue.add(stringRequest);
+    }
+
+    private void getFaultHistogram() {
+        listMonitoring.clear();
+        //OnShimmer();
+        String url = NetworkState.getUrl()+"staff/getFaultHistogram.php";
+        RequestQueue mRequestQueue = Volley.newRequestQueue(getContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("response", response);
+                try {
+                    //menangkap respon JSON dari server
+                    JSONObject jsonObject = new JSONObject(response);
+                    boolean success = jsonObject.getBoolean("success");
+                    String message = jsonObject.getString("message");
+                    //cek status login dari jsonserver
+                    if (success==true){
+                        //OnFoundData();
+
+                        String dataTotal = jsonObject.getString("dataTotal");
+
+                        JSONArray jsonArray1 = new JSONArray(dataTotal.toString());
+
+                        for (int j=0; j<jsonArray1.length(); j++){
+                            JSONObject object = jsonArray1.getJSONObject(j);
+
+//                            binding.textJumCritical.setText(object.getString("cri"));
+//                            binding.textJumMajor.setText(object.getString("maj"));
+//                            binding.textJumMinor.setText(object.getString("min"));
+
+                            listFaultHistogram.add(new PieEntry(Float.parseFloat(object.getString("totSeverity")),
+                                    object.getString("Severity")));
+                            listColors.add(randomColor());
+                        }
+
+                        PieDataSet pieDataSet = new PieDataSet(listFaultHistogram, "Fault Histogram");
+                        pieDataSet.setColors(listColors);
+
+                        PieData pieData = new PieData(pieDataSet);
+                        binding.pieChart.setData(pieData);
+                        binding.pieChart.invalidate();
+                        binding.pieChart.setDrawEntryLabels(true);
+                        binding.pieChart.setUsePercentValues(false);
+                        binding.pieChart.setCenterText("Fault");
+                        binding.pieChart.setCenterTextSize(20);
+                        binding.pieChart.setCenterTextRadiusPercent(80);
+                        binding.pieChart.setHoleRadius(30);
+                        binding.pieChart.setTransparentCircleRadius(40);
+                        binding.pieChart.setTransparentCircleColor(Color.RED);
+                        binding.pieChart.setTransparentCircleAlpha(20);
+
+                    }else{
+                        MyConfig.showToast(getContext(), message);
+                        //OnEmpty();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //OnEmpty();
+                Log.d("error", error.getMessage().toString());
+                MyConfig.showToast(getContext(), error.getMessage().toString());
+            }
+        }){
+            //parameter untuk login yang akan di tangkap api server
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                /*params.put("userIdPelayan",new SessionManager(getContext()).getIdUser());
+                params.put("stats","0");*/
+                return params;
+            }
+        };
+        int socketTimeout = 30000;
+        RetryPolicy policy = new DefaultRetryPolicy(socketTimeout, DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(policy);
+        mRequestQueue.add(stringRequest);
     }
 
     private ArrayList<BarEntry> barEntries1(){
@@ -124,5 +377,13 @@ public class HomeFaultFragment extends Fragment {
         barEntries.add(new BarEntry(7, 23234));
 
         return barEntries;
+    }
+
+    public int randomColor() {
+        Random random = new Random();
+        int color = Color.argb(255, random.nextInt(256), random.nextInt(256),
+                random.nextInt(256));
+
+        return color;
     }
 }
